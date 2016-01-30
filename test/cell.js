8fd537f472adb21460a97e8f05336816cb3309f6
Vaalentin <vaalentin@protonmail.com>
2016-01-30 17:28:12 +0100

    nest tests

--- test/cell.js
+++ test/cell.js
@@ -1,50 +1,52 @@
 import test from 'tape';
 import Cell from '../src/Cell';
 
-test('should be instanciable', t => {
-  t.plan(1);
+test('Cell', ({ test }) => {
+  test('should be instanciable', t => {
+    t.plan(1);
 
-  const cell = new Cell();
+    const cell = new Cell();
 
-  t.ok(cell instanceof Cell);
-});
+    t.ok(cell instanceof Cell);
+  });
 
-test('should expose x and y attributes', t => {
-  t.plan(2);
+  test('should expose x and y attributes', t => {
+    t.plan(2);
 
-  const cell = new Cell(2, 3);
+    const cell = new Cell(2, 3);
 
-  t.equal(cell.x, 2);
-  t.equal(cell.y, 3);
-});
+    t.equal(cell.x, 2);
+    t.equal(cell.y, 3);
+  });
 
-test('should expose a isWalkable attribute', t => {
-  t.plan(2);
+  test('should expose a isWalkable attribute', t => {
+    t.plan(2);
 
-  const cell = new Cell(2, 3);
+    const cell = new Cell(2, 3);
 
-  t.equal(cell.isWalkable, true);
+    t.equal(cell.isWalkable, true);
 
-  cell.isWalkable = false;
+    cell.isWalkable = false;
 
-  t.equal(cell.isWalkable, false);
-});
+    t.equal(cell.isWalkable, false);
+  });
 
-test('should expose a isOpen and isClosed attributes', t => {
-  t.plan(2);
+  test('should expose a isOpen and isClosed attributes', t => {
+    t.plan(2);
 
-  const cell = new Cell(2, 3);
+    const cell = new Cell(2, 3);
 
-  t.equal(cell.isOpen, false);
-  t.equal(cell.isClosed, false);
-});
+    t.equal(cell.isOpen, false);
+    t.equal(cell.isClosed, false);
+  });
 
-test('should expose the F, G and H attributes', t => {
-  t.plan(3);
+  test('should expose the F, G and H attributes', t => {
+    t.plan(3);
 
-  const cell = new Cell(2, 3);
+    const cell = new Cell(2, 3);
 
-  t.equal(cell.F, -1);
-  t.equal(cell.G, -1);
-  t.equal(cell.H, -1);
+    t.equal(cell.F, -1);
+    t.equal(cell.G, -1);
+    t.equal(cell.H, -1);
+  });
 });
--- test/grid.js
+++ test/grid.js
@@ -2,106 +2,107 @@ import test from 'tape';
 import Grid from '../src/Grid';
 import Cell from '../src/Cell';
 
-test('should be instanciable', t => {
-  t.plan(1);
+test('Grid', ({ test }) => {
+  test('should be instanciable', t => {
+    t.plan(1);
 
-  const grid = new Grid();
+    const grid = new Grid();
 
-  t.ok(grid instanceof Grid);
+    t.ok(grid instanceof Grid);
+  });
+
+  test('if dx and dy are provided, generate the cells', t => {
+    t.plan(1);
+    
+    const grid = new Grid(10, 10);
+
+    t.equal(grid._cells.length, 10 * 10);
+  });
+
+
+  test('should tell if a set of coordinates is inside the grid', t => {
+    t.plan(5);
+
+    const grid = new Grid(10, 10);
+
+    t.ok(grid.contains(5, 5));
+    t.notOk(grid.contains(15, 5));
+    t.notOk(grid.contains(5, 15));
+    t.notOk(grid.contains(-1, 5));
+    t.notOk(grid.contains(5, -1));
+  });
+
+  test('it should be possible to get a cell by coordinates', t => {
+    t.plan(3);
+
+    const grid = new Grid(10, 10);
+
+    const cell = grid.getCellAt(2, 5);
+
+    t.ok(cell instanceof Cell);
+    t.ok(cell.x, 2);
+    t.ok(cell.y, 5);
+  });
+
+  test('should returns neighbors from a set of coordinates', t => {
+    const grid = new Grid(10, 10);
+
+    const x = 5;
+    const y = 5;
+
+    // [top, topRight, right, bottomRight, bottom, bottomLeft, left, topLeft]
+    const neighbors = grid.getNeighborsAt(x, y);
+
+    t.ok(Array.isArray(neighbors));
+
+    // we have 8 neighbors since we are in the middle of the grid and all cells are walkable
+    t.equal(neighbors.length, 8); 
+
+    const coordinates = [
+      { x: x, y: y - 1 }, // top
+      { x: x + 1, y: y - 1 }, // topRight
+      { x: x + 1, y: y }, // right
+      { x: x + 1, y: 6 + 1 }, // bottomRight
+      { x: x, y: y + 1 }, // bottom
+      { x: x - 1, y: y + 1 }, // bottomLeft
+      { x: x - 1, y: y }, // left
+      { x: x - 1, y: y - 1 } // topLeft
+    ];
+
+    for(let i = 0; i < neighbors.length; i++) {
+      t.ok(neighbors[i] instanceof Cell);
+      t.equal(neighbors[i].x, coordinates[i].x);
+      t.equal(neighbors[i].y, coordinates[i].y);
+    }
+
+    t.end();
+  });
+
+  test('should ignore non walkable and offgrid neighbors', t => {
+    t.plan(2);
+
+    const grid = new Grid(10, 10);
+
+    /**
+     * x: cell
+     * 1: walkable neighbors
+     * 0: non walkable neighbors
+     */
+    const neighbors = grid.getNeighborsAt(0, 0);
+
+    /**
+     * x 1
+     * 1 1
+     */
+    t.equal(neighbors.length, 3);
+
+    grid.getCellAt(1, 0).isWalkable = false;
+    grid.getCellAt(0, 1).isWalkable = false;
+
+    /**
+     * x 0
+     * 0 1
+     */
+    t.equal(neighbors.length, 1);
+  });
 });
-
-test('if dx and dy are provided, generate the cells', t => {
-  t.plan(1);
-  
-  const grid = new Grid(10, 10);
-
-  t.equal(grid._cells.length, 10 * 10);
-});
-
-
-test('should tell if a set of coordinates is inside the grid', t => {
-  t.plan(5);
-
-  const grid = new Grid(10, 10);
-
-  t.ok(grid.contains(5, 5));
-  t.notOk(grid.contains(15, 5));
-  t.notOk(grid.contains(5, 15));
-  t.notOk(grid.contains(-1, 5));
-  t.notOk(grid.contains(5, -1));
-});
-
-test('it should be possible to get a cell by coordinates', t => {
-  t.plan(3);
-
-  const grid = new Grid(10, 10);
-
-  const cell = grid.getCellAt(2, 5);
-
-  t.ok(cell instanceof Cell);
-  t.ok(cell.x, 2);
-  t.ok(cell.y, 5);
-});
-
-test('should returns neighbors from a set of coordinates', t => {
-  const grid = new Grid(10, 10);
-
-  const x = 5;
-  const y = 5;
-
-  // [top, topRight, right, bottomRight, bottom, bottomLeft, left, topLeft]
-  const neighbors = grid.getNeighborsAt(x, y);
-
-  t.ok(Array.isArray(neighbors));
-
-  // we have 8 neighbors since we are in the middle of the grid and all cells are walkable
-  t.equal(neighbors.length, 8); 
-
-  const coordinates = [
-    { x: x, y: y - 1 }, // top
-    { x: x + 1, y: y - 1 }, // topRight
-    { x: x + 1, y: y }, // right
-    { x: x + 1, y: 6 + 1 }, // bottomRight
-    { x: x, y: y + 1 }, // bottom
-    { x: x - 1, y: y + 1 }, // bottomLeft
-    { x: x - 1, y: y }, // left
-    { x: x - 1, y: y - 1 } // topLeft
-  ];
-
-  for(let i = 0; i < neighbors.length; i++) {
-    t.ok(neighbors[i] instanceof Cell);
-    t.equal(neighbors[i].x, coordinates[i].x);
-    t.equal(neighbors[i].y, coordinates[i].y);
-  }
-
-  t.end();
-});
-
-test('should ignore non walkable and offgrid neighbors', t => {
-  t.plan(2);
-
-  const grid = new Grid(10, 10);
-
-  /**
-   * x: cell
-   * 1: walkable neighbors
-   * 0: non walkable neighbors
-   */
-  const neighbors = grid.getNeighborsAt(0, 0);
-
-  /**
-   * x 1
-   * 1 1
-   */
-  t.equal(neighbors.length, 3);
-
-  grid.getCellAt(1, 0).isWalkable = false;
-  grid.getCellAt(0, 1).isWalkable = false;
-
-  /**
-   * x 0
-   * 0 1
-   */
-  t.equal(neighbors.length, 1);
-});
-
