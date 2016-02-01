cc2fe3efccc2e74fd422eb18027716bb7502cd83
Vaalentin <vaalentin@protonmail.com>
2016-02-01 17:56:44 +0100

    add realtime path computation in example

--- example/index.js
+++ example/index.js
@@ -7,6 +7,13 @@ let ctx;
 
 let grid;
 
+let mx;
+let my;
+
+let path = [];
+
+let isMouseDown;
+
 const loop = new GameLoop(10);
 
 loop.addEventListener('init', () => {
@@ -19,17 +26,38 @@ loop.addEventListener('init', () => {
 
   grid = new Grid(10, 10);
 
-  for(let x = 0; x < 10; x++) {
-    for(let y = 0; y < 10; y++) {
-      grid.cells[x][y].isWalkable = Math.random() < 0.2 ? false : true;
-    }
-  }
+  canvas.addEventListener('mousemove', ({ offsetX: x, offsetY: y }) => {
+    mx = x / canvas.width;
+    my = y / canvas.height;
+  });
 
-  findPath(grid, 0, 0, 9, 9);
+  canvas.addEventListener('mousedown', () => isMouseDown = true);
+  canvas.addEventListener('mouseup', () => isMouseDown = false);
 });
 
 loop.addEventListener('update', () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for(let x = 0; x < grid.dx; x++) {
+    for(let y = 0; y < grid.dy; y++) {
+      const cell = grid.cells[x][y];
+
+      if(mx > (x/grid.dx) && mx < ((x+1)/grid.dx) && my > (y/grid.dy) && my < ((y+1)/grid.dy)) {
+        if(isMouseDown) {
+          cell.isWalkable = !cell.isWalkable;
+          isMouseDown = false;
+        }
+
+        path = findPath(grid, 0, 0, cell.x, cell.y);
+        
+        for(let xx = 0; xx < grid.dx; xx++) {
+          for(let yy = 0; yy < grid.dy; yy++) {
+            grid.cells[xx][yy].reset();
+          }
+        }
+      }
+    }
+  }
 
   for(let x = 0; x < grid.dx; x++) {
     for(let y = 0; y < grid.dy; y++) {
@@ -65,6 +93,18 @@ loop.addEventListener('update', () => {
       ctx.fillText('g: ' + cell.G, ax + 5, ay + 34);
     }
   }
+
+
+  // draw path
+  for(let i = 0; i < path.length; i += 2) {
+    const x = path[i];
+    const y = path[i + 1];
+    const ax = x * (canvas.width / grid.dx);
+    const ay = y * (canvas.height / grid.dy);
+    const w = canvas.width / grid.dx;
+    const h = canvas.height / grid.dy;
+  }
+
 });
 
 loop.start();
